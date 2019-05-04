9f1291c1cde7635560de2a85f3e82fd90246918b
hiredgun <turczyns@gmail.com>
2019-05-04 12:10:49 +0200

    Move prettier config to package.json

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -7,16 +7,5 @@ module.exports = {
   ],
   rules: {
     "react/require-default-props": "error",
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "lf",
-        trailingComma: "es5",
-        tabWidth: 4,
-        printWidth: 120,
-        singleQuote: true,
-        semi: true
-      }
-    ]
   }
 };
