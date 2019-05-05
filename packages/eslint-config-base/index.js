2b21a076be33894cd89a51ffb98081370acfed7f
hiredgun <turczyns@gmail.com>
2019-05-05 13:52:08 +0200

    Add initial files for eslint-config-base

---
+++ packages/eslint-config-base/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+   extends: ['react-app', 'plugin:react/recommended', 'plugin:prettier/recommended', 'prettier/react'],
+   rules: {
+      'react/require-default-props': 'error',
+   },
+};
