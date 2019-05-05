ce390f309ab7cf4bae03088a0e08844bd94f383d
hiredgun <turczyns@gmail.com>
2019-05-05 17:40:40 +0200

    Add base setup for base eslint config

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,5 +1,11 @@
 module.exports = {
-   extends: ['react-app', 'plugin:react/recommended', 'plugin:prettier/recommended', 'prettier/react'],
+   extends: [
+      'kentcdodds',
+      'kentcdodds/jest',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
+      'prettier/react',
+   ],
    rules: {
       'react/require-default-props': 'error',
    },
